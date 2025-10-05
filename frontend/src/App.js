62fb7e1187af219009cd84d57e8f789323beb0f8
Nishant Jha <145684364+nishant-jha72@users.noreply.github.com>
2025-10-05 23:09:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -104,6 +104,10 @@ function App() {
             </div>
           ))}
         </div>
+        <video className="background-video" autoPlay loop muted>
+  <source src={`${process.env.PUBLIC_URL}/background.mp4`} type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
 
         {/* Completed Tasks */}
         <div className="task-section">
