3192cfbab1f8fc39b241299934250bf0d315a60b
Nishant Jha <145684364+nishant-jha72@users.noreply.github.com>
2025-10-05 20:03:50 +0530

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+const taskRoutes = require("./routes/tasks");
+
 
 // Get all tasks
 router.get("/", async (req, res) => {
